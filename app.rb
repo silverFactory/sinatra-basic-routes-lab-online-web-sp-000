a51ff5d6dda425b680cf8afc87fafcf0e3e08ca6
Will Studabaker <will.studabaker@gmail.com>
2020-12-11 15:18:15 -0500

    commit tree 38890986849df37058688c3598f6f0ddce766505

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name' do
+  "My name is Will"
+  end
+
+  get '/hometown' do
+  "My hometown is Fairfax, VA"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is Black Mamba by Aespa"
+  end
 end
